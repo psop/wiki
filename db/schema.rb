7b5fc5541ed9d8adb12b6a994ed351849e3a9a44
LizHuang <search.psop@gmail.com.tw>
2017-06-08 23:50:43 +1000

    Add Categories to Articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608120349) do
+ActiveRecord::Schema.define(version: 20170608133541) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
